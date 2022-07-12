46542b3b7a795eb68e53eed68a192ad21a310dc3
Rishabh <rish2001.ra@gmail.com>
2022-07-11 21:33:34 -0500

    created a class

--- Code1.py
+++ Code1.py
@@ -1,1 +1,19 @@
+import pandas as pd
+
+class Data:
+    def __init__(file,data):
+        file.date = data['Date']
+        file.open = data['Open']
+        file.high = data['High']
+        file.low = data['Low']
+        file.close = data['Close']
+        file.adj_close = data['Adj Close']
+        file.volume = data['Volume']
+        
+
+df_apple = pd.read_csv("AAPL.csv")
+df_google = pd.read_csv("GOOG.csv")
+
+appledata = Data(df_apple)
+googledata = Data(df_google)
 
