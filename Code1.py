08072478f65ecdafdf8485500cb1ad01da23441a
Rishabh <rish2001.ra@gmail.com>
2022-07-14 21:08:39 -0500

    ema

--- Code1.py
+++ Code1.py
@@ -1,5 +1,5 @@
-from turtle import color
 import pandas as pd
+import pandas_ta as ta
 import matplotlib.pyplot as plt
 
 class Data:
@@ -16,9 +16,21 @@ class Data:
 df_apple = pd.read_csv("AAPL.csv")
 df_google = pd.read_csv("GOOG.csv")
 
+
+df_close = df_apple[['Close']]
+df_close.set_index(pd.DatetimeIndex(df_apple['Date']),inplace = True)
 appledata = Data(df_apple)
 googledata = Data(df_google)
 
+df_apple.plot('Date','Close',color = 'red')
 
-df_google.plot('Date','Close',color = 'red')
+
+df_close.ta.ema(close ='Close',length =10,append =True)
+
+df_close = df_close.iloc[10:]
+df_close.tail(1000).plot(kind='line')
+
 plt.show()
+
+
+
