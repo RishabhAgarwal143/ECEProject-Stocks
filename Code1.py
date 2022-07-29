18ac93b0d4d7bb21cd2d0a2aa8df932420f7f7c0
Neha Sharma <nehasharma.c2021@gmail.com>
2022-07-29 09:09:13 -0400

    Added MAE for training data

--- Code1.py
+++ Code1.py
@@ -61,12 +61,14 @@ plt.show()
 
 #Google linear reg model
 def google_model():
-    Xtrain, Xtest, ytrain, ytest = train_test_split(df_close_google[['EMA_10']], df_close_google[['Close']], random_state = 101)
+    Xtrain, Xtest, ytrain, ytest = train_test_split(df_close_google[['EMA_10']], df_close_google[['Close']], test_size = 0.1,random_state = 101)
 
     Gmodel = LinearRegression(fit_intercept=False).fit(Xtrain,ytrain)
     Gpred = Gmodel.predict(Xtest)
+    Gtrain = Gmodel.predict(Xtrain)
     print("Google Model Coefficients:", Gmodel.coef_)
-    print("Mean Absolute Error:", mean_absolute_error(ytest, Gpred))
+    print("Mean Absolute Error for Testing data:", mean_absolute_error(ytest, Gpred))
+    print("Mean Absolute Error for Training data:", mean_absolute_error(ytrain, Gtrain))
     print("Coefficient of Determination:", r2_score(ytest, Gpred))
 
     yG = [(Gmodel.coef_[0][0])*i for i in Xtest['EMA_10']]
@@ -84,12 +86,14 @@ def google_model():
 
 #Apple Model
 def apple_model():
-    Xtrain, Xtest, ytrain, ytest = train_test_split(df_close_apple[['EMA_10']], df_close_apple[['Close']], random_state = 101)
+    Xtrain, Xtest, ytrain, ytest = train_test_split(df_close_apple[['EMA_10']], df_close_apple[['Close']], test_size = 0.1, random_state = 101)
 
     Amodel = LinearRegression(fit_intercept=False).fit(Xtrain,ytrain)
+    Atrain = Amodel.predict(Xtrain)
     Apred = Amodel.predict(Xtest)
     print("Apple Model Coefficients:", Amodel.coef_)
-    print("Mean Absolute Error:", mean_absolute_error(ytest, Apred))
+    print("Mean Absolute Error for Testing Data:", mean_absolute_error(ytest, Apred))
+    print("Mean Absolute Error for Training data:", mean_absolute_error(ytrain, Atrain))
     print("Coefficient of Determination:", r2_score(ytest, Apred))
 
     yG = [(Amodel.coef_[0][0])*i for i in Xtest['EMA_10']]
